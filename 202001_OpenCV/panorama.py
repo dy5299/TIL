f73463daa9c2c2bb8fbacdb90c256f96402a1543
dy5299 <dykgit@gmail.com>
2020-01-14 17:55:41 +0900

    2001141755

---
+++ 202001_OpenCV/panorama.py
@@ -0,0 +1,68 @@
+import cv2, numpy as np
+import math
+import time
+import random
+from matplotlib import pyplot as plt
+import imutils
+import qrcode
+#from pyzbar.pyzbar import decode
+from bokeh.plotting import figure
+from bokeh.io import output_notebook, show, push_notebook
+
+
+output_notebook()   #노트북 업데이트 (내부함수)
+
+
+def imshow(tit, image) :
+    plt.title(tit)    
+    if len(image.shape) == 3 :
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    else :
+        plt.imshow(image, cmap="gray")
+    plt.show()
+    
+    
+def create_win(frames, scale=1.0) :    
+    global myImage
+    
+    all = []
+    for f in frames :
+        if len(f.shape ) !=  3 : f = cv2.cvtColor(f, cv2.COLOR_GRAY2BGR)
+        all.append(f)
+    frame = np.vstack(all)
+    
+    fr=cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA) # because Bokeh expects a RGBA image
+    fr=cv2.flip(fr, -1) # because Bokeh flips vertically
+    width=fr.shape[1]
+    height=fr.shape[0]    
+
+    p = figure(x_range=(0,width), y_range=(0,height), output_backend="webgl", width=int(width*scale), height=int(height*scale))    
+    myImage = p.image_rgba(image=[fr], x=0, y=0, dw=width, dh=height)
+    show(p, notebook_handle=True)   
+    
+    
+def update_win(frames) :
+    
+    all = []
+    for f in frames :
+        if len(f.shape ) !=  3 : f = cv2.cvtColor(f, cv2.COLOR_GRAY2BGR)
+        all.append(f)
+    frame = np.vstack(all)
+    
+    fr=cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    fr=cv2.flip(fr, -1)
+    myImage.data_source.data['image']=[fr]
+    push_notebook()
+    time.sleep(0.05)
+
+
+files = ["img/img1.jpg","img/img2.jpg","img/img3.jpg"]
+#files=["left.jpeg","right.jpeg"]
+imgs=[]
+for file in files:
+    img=cv2.imread(file)
+    imgs.append(img)
+
+stitcher = cv2.Stitcher_create()
+status, dst = stitcher.stitch(imgs)
+cv2.imwrite("result.jpg", dst)
