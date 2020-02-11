621ee74bb2cc610e6e25c7f34b6d212c18f9c46b
dy5299 <dykgit@gmail.com>
2020-02-11 17:21:38 +0900

    html 통신

---
+++ 202002/test2_template_test.py
@@ -0,0 +1,29 @@
+html = """
+<html>
+   <head>
+      <meta charset="utf-8">
+   </head>
+
+   <body>
+      <font color=red> @out</font>
+
+      <table border=1>
+         <tr>
+         <td> 이름 </td> <td>@name</td>
+         </tr>
+         <tr>
+         <td> email </td> <td>@email</td>
+         </tr>
+      </table>
+
+
+   </body>
+</html>
+"""
+
+html = html.replace("@out",  "제목출력")
+html = html.replace("@name",  "이순신")
+html = html.replace("@email",  "lee@gmail.com")
+#template engine에서 실시간으로 바꿔준다.
+
+print(html)
