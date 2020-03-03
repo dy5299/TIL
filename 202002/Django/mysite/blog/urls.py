e34c049010c23091d9fe2f242f299ec08aacb3c4
dy5299 <dykgit@gmail.com>
2020-03-03 17:24:00 +0900

    edit form

--- 202002/Django/mysite/blog/urls.py
+++ 202002/Django/mysite/blog/urls.py
@@ -6,9 +6,10 @@ urlpatterns = [
 #    path('<name>/', views.index2),      #<name> parameter에 대해 동적으로 mapping
 #    path('<int:pk>/detail', views.index3),
 
-    path('login/', views.LoginView.as_view(), name='login'),  # class base
+    path('login/', views.LoginView.as_view(), name='login'),    # class base
 
-    path('list', views.list, name='list'),
-    path('<int:pk>/detail', views.detail, name='detail'),       #function base
+    path('list/', views.list, name='list'),
+    path('<int:pk>/detail/', views.detail, name='detail'),       #function base
     path('add/', views.PostView.as_view(), name='add'),
+    path('<int:pk>/edit/', views.PostEditView.as_view(), name='edit'),
 ]
--- 202002/Django/mysite/blog/views.py
+++ 202002/Django/mysite/blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from blog.models import Post
 from django.views.generic import View
-from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
+from django.forms import Form, CharField, Textarea, ValidationError
 
 # Create your views here.
 
@@ -30,19 +31,6 @@ def detail(request, pk):
     return render(request, "blog/detail.html", {"d":p})
 
 
-class PostView(View):
-    def get(self, request):
-        return render(request, "blog/edit.html")
-
-    def post(self, request):
-        title = request.POST.get('title')
-        text = request.POST.get('text')
-        username = request.session['username']
-        user = User.objects.get(username=username)
-        Post.objects.create(title=title, text=text, author=user)    #create:생성과 동시에 save
-
-        return redirect('list')
-
 
 class LoginView(View):
     def get(self, request):
@@ -59,3 +47,46 @@ class LoginView(View):
         #로그인 성공한 경우
         request.session['username'] = username
         return redirect('list')
+
+
+
+class PostView(View):
+    def get(self, request):
+        return render(request, "blog/edit.html")
+
+    def post(self, request):
+        title = request.POST.get('title')
+        text = request.POST.get('text')
+        username = request.session['username']
+        user = User.objects.get(username=username)
+        Post.objects.create(title=title, text=text, author=user)    #create:생성과 동시에 save
+        return redirect('list')
+
+
+
+
+def validator(value):
+    if len(value) < 5 : raise ValidationError("길이가 너무 짧아요");
+
+class PostForm(Form):
+    title = CharField(label='제목', max_length=20, validators=[validator])
+    text = CharField(label='내용', widget=Textarea)
+
+class PostEditView(View):
+    def get(self, request, pk):     #특정 포스트를 수정하므로 pk parameter를 받아와야 한다.
+        #초기값 지정
+        post = get_object_or_404(Post, pk=pk)
+        form = PostForm(initial={'title':post.title, 'text':post.text})
+        return render(request, "blog/edit.html", {'form':form, 'pk':pk})
+
+    def post(self, request, pk):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = get_object_or_404(Post, pk=pk)
+            post.title = form['title'].value()
+            post.text = form['text'].value()
+            post.publish()
+            return redirect('list')
+        return render(request, 'blog/edit.html', {'form':form, 'pk':pk})
+
+
